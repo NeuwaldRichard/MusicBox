5ebd92c214d07f4cd9467cc11a567823e61cc700
Richard Neuwald <119449353+Neuwald-Ric@users.noreply.github.com>
2024-09-01 21:00:21 -0300

    Adicionar arquivos de projeto.

---
+++ MusicBox/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+string mensagemDeBoasVinda = "Seja Bem Vindo";
+
+void ExibirBoasVindas()
+{
+    Console.WriteLine(@"
+███╗░░░███╗██╗░░░██╗░██████╗██╗░█████╗░  ██████╗░░█████╗░██╗░░██╗
+████╗░████║██║░░░██║██╔════╝██║██╔══██╗  ██╔══██╗██╔══██╗╚██╗██╔╝
+██╔████╔██║██║░░░██║╚█████╗░██║██║░░╚═╝  ██████╦╝██║░░██║░╚███╔╝░
+██║╚██╔╝██║██║░░░██║░╚═══██╗██║██║░░██╗  ██╔══██╗██║░░██║░██╔██╗░
+██║░╚═╝░██║╚██████╔╝██████╔╝██║╚█████╔╝  ██████╦╝╚█████╔╝██╔╝╚██╗
+╚═╝░░░░░╚═╝░╚═════╝░╚═════╝░╚═╝░╚════╝░  ╚═════╝░░╚════╝░╚═╝░░╚═╝
+");
+    Console.WriteLine(mensagemDeBoasVinda);   
+}
+ExibirBoasVindas();
+
+void ExibirMenu()
+{
+    Console.WriteLine("\nDigite 1 para registrar uma banda.");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas.");
+    Console.WriteLine("Digite 3 para avaliar uma banda.");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda.");
+    Console.WriteLine("Digite 0 para sair.");
+}
+ExibirMenu();
+
+Console.Write("\nDigite Sua Opção: ");
+string opcaoEscolhida = Console.ReadLine();
+int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+if (opcaoEscolhidaNumerica == 1)
+{
+
+    Console.WriteLine( "Você digitou a opção " + opcaoEscolhida );
+}
